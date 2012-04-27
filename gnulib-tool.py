1c6af6ad40de0fbea2868775dbcf63e81c34ac9f
ghostmansd <ghostmansd@gmail.com>
2012-04-27 04:01:05 +0400

    27 Apr 2012, Dmitriy Selyutin APPS variable is now called UTILS. Some variables like __appname__, __appath__ and __appdir__ were replaced to global APP dictionary, which now contains args 'name' and 'path'. New global variable DIRS was created to store full paths of the main directory and subdirectories. New GNULibInfo class was created; it contains some useful methods like name, authors, license and copyright. Don't be confused with some similar variables like __author__, __license__, etc. The new methods and variables are similar, but they are not the same: methods will provide user information which will be mostly used in stdout messages. Later I am going to add version method, which will extract versions of script and from git (or CVS if we will use it in the future). You can see almost completed stdout message which you can get from (gnulib-tool.sh --version). To see it just run (python gnulib-tool.py).

--- gnulib-tool.py
+++ gnulib-tool.py
@@ -1,31 +1,49 @@
 #!/usr/bin/python3
 
-################################################################################
+####################################################################################################
 # Define global imports
-################################################################################
+####################################################################################################
 import os
 import re
 import sys
 import subprocess
 
-################################################################################
+####################################################################################################
 # Define module information
-################################################################################
-__appname__ = 'gnulib-tool'
-__apppath__ = os.path.realpath(sys.argv[0])
-__appdir__ = os.path.dirname(__apppath__)
+####################################################################################################
+__all__ = ['GNULibInfo']
+__license__ = 'GNU GPLv3+'
+__copyright__ = '2012 Free Software Foundation, Inc.'
 __author__ = \
 [
   'Bruno Haible',
   'Simon Josefsson',
   'Dmitriy Selyutin',
 ]
-__license__ = 'GNU GPLv3+'
 __version__ = 0.1
 
-################################################################################
+####################################################################################################
 # Define global constants
-################################################################################
+####################################################################################################
+# Declare neccessary variables
+APP = dict() # Application
+DIRS = dict() # Directories
+UTILS = dict() # Utilities
+
+# Set APP dictionary
+APP['name'] = os.path.basename(sys.argv[0])
+APP['path'] = os.path.realpath(sys.argv[0])
+
+# Set DIRS dictionary
+DIRS['root'] = os.path.dirname(APP['path'])
+DIRS['build-aux'] = os.path.join(DIRS['root'], 'build-aux')
+DIRS['config'] = os.path.join(DIRS['root'], 'config')
+DIRS['doc'] = os.path.join(DIRS['root'], 'doc')
+DIRS['lib'] = os.path.join(DIRS['root'], 'lib')
+DIRS['m4'] = os.path.join(DIRS['root'], 'm4')
+DIRS['modules'] = os.path.join(DIRS['root'], 'modules')
+DIRS['tests'] = os.path.join(DIRS['root'], 'tests')
+
 # You can set AUTOCONFPATH to empty if autoconf 2.57 is already in your PATH
 AUTOCONFPATH = ''
 # You can set AUTOMAKEPATH to empty if automake 1.9.x is already in your PATH
@@ -35,79 +53,120 @@ GETTEXTPATH = ''
 # You can set LIBTOOLPATH to empty if libtoolize 2.x is already in your PATH
 LIBTOOLPATH = ''
 
-APPS = dict() # Create empty dictionary
-
 # You can also set the variable AUTOCONF individually
 if AUTOCONFPATH:
-  APPS['autoconf'] = AUTOCONFPATH + 'autoconf'
+  UTILS['autoconf'] = AUTOCONFPATH + 'autoconf'
 else:
   if os.getenv('AUTOCONF'):
-    APPS['autoconf'] = os.getenv('AUTOCONF')
+    UTILS['autoconf'] = os.getenv('AUTOCONF')
   else:
-    APPS['autoconf'] = 'autoconf'
+    UTILS['autoconf'] = 'autoconf'
 
 # You can also set the variable AUTORECONF individually
 if AUTOCONFPATH:
-  APPS['autoreconf'] = AUTOCONFPATH + 'autoreconf'
+  UTILS['autoreconf'] = AUTOCONFPATH + 'autoreconf'
 else:
   if os.getenv('AUTORECONF'):
-    APPS['autoreconf'] = os.getenv('AUTORECONF')
+    UTILS['autoreconf'] = os.getenv('AUTORECONF')
   else:
-    APPS['autoreconf'] = 'autoreconf'
+    UTILS['autoreconf'] = 'autoreconf'
 
 # You can also set the variable AUTOHEADER individually
 if AUTOCONFPATH:
-  APPS['autoheader'] = AUTOCONFPATH + 'autoheader'
+  UTILS['autoheader'] = AUTOCONFPATH + 'autoheader'
 else:
   if os.getenv('AUTOHEADER'):
-    APPS['autoheader'] = os.getenv('AUTOHEADER')
+    UTILS['autoheader'] = os.getenv('AUTOHEADER')
   else:
-    APPS['autoheader'] = 'autoheader'
+    UTILS['autoheader'] = 'autoheader'
 
 # You can also set the variable AUTOMAKE individually
 if AUTOMAKEPATH:
-  APPS['automake'] = AUTOMAKEPATH + 'automake'
+  UTILS['automake'] = AUTOMAKEPATH + 'automake'
 else:
   if os.getenv('AUTOMAKE'):
-    APPS['automake'] = os.getenv('AUTOMAKE')
+    UTILS['automake'] = os.getenv('AUTOMAKE')
   else:
-    APPS['automake'] = 'automake'
+    UTILS['automake'] = 'automake'
 
 # You can also set the variable ACLOCAL individually
 if AUTOMAKEPATH:
-  APPS['aclocal'] = AUTOMAKEPATH + 'aclocal'
+  UTILS['aclocal'] = AUTOMAKEPATH + 'aclocal'
 else:
   if os.getenv('ACLOCAL'):
-    APPS['aclocal'] = os.getenv('ACLOCAL')
+    UTILS['aclocal'] = os.getenv('ACLOCAL')
   else:
-    APPS['aclocal'] = 'aclocal'
+    UTILS['aclocal'] = 'aclocal'
 
 # You can also set the variable AUTOPOINT individually
 if GETTEXTPATH:
-  APPS['autopoint'] = GETTEXTPATH + 'autopoint'
+  UTILS['autopoint'] = GETTEXTPATH + 'autopoint'
 else:
   if os.getenv('AUTOPOINT'):
-    APPS['autopoint'] = os.getenv('AUTOPOINT')
+    UTILS['autopoint'] = os.getenv('AUTOPOINT')
   else:
-    APPS['autopoint'] = 'autopoint'
+    UTILS['autopoint'] = 'autopoint'
 
 # You can also set the variable LIBTOOLIZE individually
 if LIBTOOLPATH:
-  APPS['libtoolize'] = LIBTOOLPATH + 'libtoolize'
+  UTILS['libtoolize'] = LIBTOOLPATH + 'libtoolize'
 else:
   if os.getenv('LIBTOOLIZE'):
-    APPS['libtoolize'] = os.getenv('LIBTOOLIZE')
+    UTILS['libtoolize'] = os.getenv('LIBTOOLIZE')
   else:
-    APPS['libtoolize'] = 'libtoolize'
+    UTILS['libtoolize'] = 'libtoolize'
 
 # You can also set the variable MAKE
 if os.getenv('MAKE'):
-  APPS['make'] = os.getenv('MAKE')
+  UTILS['make'] = os.getenv('MAKE')
 else:
-  APPS['make'] = 'make'
+  UTILS['make'] = 'make'
 
 # Delete temporary variables
 del(AUTOCONFPATH)
 del(AUTOMAKEPATH)
 del(GETTEXTPATH)
 del(LIBTOOLPATH)
+
+####################################################################################################
+# Define GNULibInfo class
+####################################################################################################
+class GNULibInfo(object):
+  '''This class is used to get fromatted information about gnulib-tool.
+  This information is mainly used in stdout messages, but can be used
+  anywhere else. The return values are not the same as for the module,
+  but still depends on them.'''
+  
+  def name(self):
+    result = 'GNU gnulib-tool'
+    return(result)
+    
+  def authors(self):
+    '''Return formatted string which contains authors.
+    The special __author__ variable is used (type is list).'''
+    result = str() # Empty string
+    for item in __author__:
+      if item == __author__[-2]:
+        result += '%s ' % item
+      elif item == __author__[-1]:
+        result += 'and %s' % item
+      else:
+        result += '%s, ' % item
+    return(result)
+    
+  def license(self):
+    result = 'License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n'
+    result += 'This is free software: you are free to change and redistribute it.\n'
+    result += 'There is NO WARRANTY, to the extent permitted by law.'
+    return(result)
+    
+  def copyright(self):
+    result = 'Copyright (C) %s' % __copyright__
+    return(result)
+
+if __name__ == '__main__':
+  info = GNULibInfo()
+  message = \
+    '%s (here will be gnulib-tool version)\n%s\n%s\n\n%s' % \
+    (info.name(), info.copyright(), info.license(), info.authors())
+  print(message)
